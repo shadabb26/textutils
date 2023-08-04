e875a1dc85edc155ae5f59cece102f6b7d994e3a
shadabb26 <shadabshikalgar2003@gmail.com>
2023-08-04 19:09:17 +0530

    bug fixes

--- src/Components/Alert.js
+++ src/Components/Alert.js
@@ -7,10 +7,10 @@ export default function Alert(props) {
  }
 
   return (
-
-      props.alert &&<div className={`alert alert-${props.alert.type} alert-dismissible fade show role="alert" my-1`}>
+      <div style={{height:'42px'}}>
+      {props.alert &&<div className={`alert alert-${props.alert.type} alert-dismissible fade show role="alert"`}>
   <strong>{capatalize(props.alert.type)}! </strong>{props.alert.msg}
-
+  </div>}
 </div>
     
   )
--- src/Components/Textarea.js
+++ src/Components/Textarea.js
@@ -1,8 +1,9 @@
+import { isDisabled } from '@testing-library/user-event/dist/utils';
 import React,{useState} from 'react';
 
 
 export default function Textform(props) {
-    const [text,set]=useState('Developed by Shadabb');
+    const [text,set]=useState('Try textutils - Word Counting, Character Counting, Remove Extra Spaces, Text to UpperCase, Text to LowerCase\nDeveloped By Shadabb');
 
     const handleclick=()=>{
      let newtext=text.toUpperCase();
@@ -22,22 +23,22 @@ export default function Textform(props) {
     set(newtextt);
     props.showAlert("Converted To LowerCase","success")
    }
-   const count=(text)=>{
-     let nofwords=text.split(" ").length
-     if(nofwords===1){
-      nofwords=text.split(" ").length-1;
-      return nofwords;
-     }
-     else if(text.split(" ").length===" ")
-     {
-     return nofwords-1;
-     }
+  //  const count=(text)=>{
+  //    let nofwords=text.split(" ").length
+  //    if(nofwords==1){
+  //     nofwords=text.split(" ").length-1;
+  //     return nofwords;
+  //    }
+  //    else if(text.split(" ").length==" ");{
+  //    return nofwords-1;
+  //    }
     
-   }
+  //  }
 
    const copy=()=>{
     let txt=document.getElementById('exampleFormControlTextarea1')
     txt.select();
+    document.getSelection().removeAllRanges();
     navigator.clipboard.writeText(txt.value)
     props.showAlert("Copied to Clipboard","success")
    }
@@ -50,19 +51,20 @@ export default function Textform(props) {
     
       <div className="mb-3 my-4">
         <textarea className="form-control" id="exampleFormControlTextarea1" rows="6" value={text} onChange={(event)=>onchange(event)}></textarea>
-        <button className="btn btn-primary my-2" onClick={()=>handleclick()}>To Upper Case</button>
-        <button className="btn btn-primary my-2 mx-2" onClick={()=>handleloclick()}>To Lower Case</button>
-        <button className="btn btn-primary my-2 " onClick={()=>copy()}>Copy Text</button>
-        <button className="btn btn-primary my-2 mx-2" onClick={()=>extraspace()}>Remove Extra Spaces</button>
-        <button className="btn btn-danger my-2" onClick={()=>clear()}>Clear</button>
+        <button disabled={text.length===0} className="btn btn-primary my-2" onClick={()=>handleclick()}>To Upper Case</button>
+        <button disabled={text.length===0} className="btn btn-primary my-2 mx-2" onClick={()=>handleloclick()}>To Lower Case</button>
+        <button disabled={text.length===0} className="btn btn-primary my-2 " onClick={()=>copy()}>Copy Text</button>
+        <button disabled={text.length===0} className="btn btn-primary my-2 mx-2" onClick={()=>extraspace()}>Remove Extra Spaces</button>
+        <button disabled={text.length===0} className="btn btn-danger my-2" onClick={()=>clear()}>Clear</button>
       </div>
 
       <div className={`container text-${props.mode==='dark'?'light':'dark'}`}>
         <h2>Text summary</h2>
-        <p>Number of words {count(text)} & Number of Characters {text.length}</p>
+        <p>Number of words {text.split(" ").filter((element)=>{return element.length!==0}).length} & Number of Characters {text.length}</p>
         <p>Time to read the text is {Math.round(0.008*text.split(" ").length)} Minutes</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:'Nothing to Preview'}</p>
+        
       </div>
       </>
   ); 
